012cc02a13865d50d0fb9dea5f685c0bf10cc91a
shanureddy4 <shanureddy78@gmail.com>
2022-04-14 13:56:27 +0530

    application file changed

--- src/main/java/com/example/kaiburr/KaiburrApplication.java
+++ src/main/java/com/example/kaiburr/KaiburrApplication.java
@@ -1,9 +1,13 @@
 package com.example.kaiburr;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class KaiburrApplication {
@@ -11,7 +15,15 @@ public class KaiburrApplication {
     public static void main(String[] args) {
         SpringApplication.run(KaiburrApplication.class, args);
     }
-
+    @Bean
+    public TomcatServletWebServerFactory tomcatFactory() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+            }
+        };
+    }
 
 }
 
